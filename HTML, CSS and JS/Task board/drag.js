df19fa6dc8c792c9277903227d0eba610ad2059e
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2024-08-25 17:22:16 +0200

    Update all
    Format with prettier

--- HTML, CSS and JS/Task board/drag.js
+++ HTML, CSS and JS/Task board/drag.js
@@ -1,51 +1,51 @@
 const draggables = document.querySelectorAll(".task");
 const droppables = document.querySelectorAll(".swim-lane");
 
-import {save} from "/HTML, CSS and JS/Task board/todo.js";
+import { save } from "/HTML, CSS and JS/Task board/todo.js";
 
 draggables.forEach((task) => {
-  task.addEventListener("dragstart", (e) => {
-    task.classList.add("is-dragging");
-    console.log(e);
-  });
-  task.addEventListener("dragend", () => {
-    task.classList.remove("is-dragging");
-    save()
-  });
+    task.addEventListener("dragstart", (e) => {
+        task.classList.add("is-dragging");
+        console.log(e);
+    });
+    task.addEventListener("dragend", () => {
+        task.classList.remove("is-dragging");
+        save();
+    });
 });
 
 droppables.forEach((zone) => {
-  zone.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    const bottomTask = insertAboveTask(zone, e.clientY);
-    const curTask = document.querySelector(".is-dragging");
+    zone.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const bottomTask = insertAboveTask(zone, e.clientY);
+        const curTask = document.querySelector(".is-dragging");
 
-    if (!bottomTask) {
-      zone.appendChild(curTask);
-      save()
-    } else {
-      zone.insertBefore(curTask, bottomTask);
-      save()
-    }
-  });
+        if (!bottomTask) {
+            zone.appendChild(curTask);
+            save();
+        } else {
+            zone.insertBefore(curTask, bottomTask);
+            save();
+        }
+    });
 });
 
 const insertAboveTask = (zone, mouseY) => {
-  const els = zone.querySelectorAll(".task:not(.is-dragging)");
+    const els = zone.querySelectorAll(".task:not(.is-dragging)");
 
-  let closestTask = null;
-  let closestoffset = Number.NEGATIVE_INFINITY;
+    let closestTask = null;
+    let closestoffset = Number.NEGATIVE_INFINITY;
 
-  els.forEach((task) => {
-    const { top } = task.getBoundingClientRect();
+    els.forEach((task) => {
+        const { top } = task.getBoundingClientRect();
 
-    const offset = mouseY - top;
+        const offset = mouseY - top;
 
-    if (offset < 0 && offset > closestoffset) {
-      closestoffset = offset;
-      closestTask = task;
-    }
-  });
+        if (offset < 0 && offset > closestoffset) {
+            closestoffset = offset;
+            closestTask = task;
+        }
+    });
 
-  return closestTask;
+    return closestTask;
 };
--- HTML, CSS and JS/Task board/todo.js
+++ HTML, CSS and JS/Task board/todo.js
@@ -10,54 +10,54 @@ const clearDoneBtn = document.getElementById("clear-done");
 let task = [];
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const value1 = input1.value;
-  const value2 = input2.value;
-  if (!value1) return;
-  if (!value2) return;
+    e.preventDefault();
+    const value1 = input1.value;
+    const value2 = input2.value;
+    if (!value1) return;
+    if (!value2) return;
 
-  const newTask = document.createElement("div");
-  newTask.classList.add("task");
-  newTask.setAttribute("draggable", "true");
-  newTask.innerHTML = `<h1 class="task-title">${value1}</h1>
+    const newTask = document.createElement("div");
+    newTask.classList.add("task");
+    newTask.setAttribute("draggable", "true");
+    newTask.innerHTML = `<h1 class="task-title">${value1}</h1>
                        <p class="task-desc">${value2}</p>`;
 
-  newTask.addEventListener("dragstart", () => {
-    newTask.classList.add("is-dragging");
-  });
-  newTask.addEventListener("dragend", () => {
-    newTask.classList.remove("is-dragging");
-  });
+    newTask.addEventListener("dragstart", () => {
+        newTask.classList.add("is-dragging");
+    });
+    newTask.addEventListener("dragend", () => {
+        newTask.classList.remove("is-dragging");
+    });
 
-  todolane.appendChild(newTask);
+    todolane.appendChild(newTask);
 
-  input1.value = "";
-  input2.value = "";
+    input1.value = "";
+    input2.value = "";
 });
 
 function save() {
-  localStorage.clear();
-  localStorage.setItem("todo", todolane.innerHTML);
-  localStorage.setItem("prog", proglane.innerHTML);
-  localStorage.setItem("done", donelane.innerHTML);
-  localStorage.setItem("rev", reviewlane.innerHTML);
-};
+    localStorage.clear();
+    localStorage.setItem("todo", todolane.innerHTML);
+    localStorage.setItem("prog", proglane.innerHTML);
+    localStorage.setItem("done", donelane.innerHTML);
+    localStorage.setItem("rev", reviewlane.innerHTML);
+}
 
 function clear() {
-  localStorage.clear();
-  localStorage.setItem("todo", `<h3 class="heading">To do</h3>`);
-  localStorage.setItem("prog", `<h3 class="heading">In progress</h3>`);
-  localStorage.setItem("done", `<h3 class="heading">Done</h3>`);
-  localStorage.setItem("rev", `<h3 class="heading">Under review</h3>`);
-  todolane.innerHTML = `<h3 class="heading">To do</h3>`;
-  proglane.innerHTML = `<h3 class="heading">In progress</h3>`;
-  donelane.innerHTML = `<h3 class="heading">Done</h3>`;
-  reviewlane.innerHTML = `<h3 class="heading">Under review</h3>`;
-};
+    localStorage.clear();
+    localStorage.setItem("todo", `<h3 class="heading">To do</h3>`);
+    localStorage.setItem("prog", `<h3 class="heading">In progress</h3>`);
+    localStorage.setItem("done", `<h3 class="heading">Done</h3>`);
+    localStorage.setItem("rev", `<h3 class="heading">Under review</h3>`);
+    todolane.innerHTML = `<h3 class="heading">To do</h3>`;
+    proglane.innerHTML = `<h3 class="heading">In progress</h3>`;
+    donelane.innerHTML = `<h3 class="heading">Done</h3>`;
+    reviewlane.innerHTML = `<h3 class="heading">Under review</h3>`;
+}
 
 function clearDone() {
-  localStorage.setItem("done", `<h3 class="heading">Done</h3>`)
-  donelane.innerHTML = `<h3 class="heading">Done</h3>`;
+    localStorage.setItem("done", `<h3 class="heading">Done</h3>`);
+    donelane.innerHTML = `<h3 class="heading">Done</h3>`;
 }
 
 clearBtn.addEventListener("click", clear);
@@ -69,13 +69,12 @@ donelane.innerHTML = localStorage.getItem("done");
 reviewlane.innerHTML = localStorage.getItem("rev");
 task = document.querySelectorAll("div.task");
 for (let i = 0; i < task.length; i++) {
-  task[i].addEventListener("dragstart", () => {
-    task[i].classList.add("is-dragging");
-  });
-  task[i].addEventListener("dragend", () => {
-    task[i].classList.remove("is-dragging");
-  });
-};
+    task[i].addEventListener("dragstart", () => {
+        task[i].classList.add("is-dragging");
+    });
+    task[i].addEventListener("dragend", () => {
+        task[i].classList.remove("is-dragging");
+    });
+}
 
-export {save}
-
+export { save };
