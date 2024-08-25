80a5238de73340b73987fa9bcb5f174203cea3bd
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2024-08-26 01:31:38 +0200

    Update 4 main files
    Basic functionality of main page and move dog info to separate file

---
+++ HTML, CSS and JS/FurEver Friends/index.js
@@ -0,0 +1,28 @@
+const petList = document.getElementById("pet-list");
+
+import {bichonFrise, miniPoodle} from "/HTML, CSS and JS/FurEver Friends/info.js"
+
+function createCard(name) {
+    const li = document.createElement("li");
+    li.classList.add("pet-card");
+    li.innerHTML = `<img src="${name.src}" class="pet-img">
+                <div class="text-div">
+                    <h1 class="name">${name.name}<img src="img/svg/${name.gender}_24dp_BLACK_FILL1_wght400_GRAD0_opsz24.svg" class="gender"></h1>
+                    <p class="breed">${name.breed}</p>
+                    <p class="distance"><img src="img/svg/location_on_24dp_D16D6A_FILL1_wght400_GRAD0_opsz24.svg" class="location">${name.distance}</p>
+                </div>
+                <button class="like-btn"><img id="${name.name}-btn" src="" class="black like"></button>`;
+
+
+    petList.appendChild(li);
+    
+        function changeLikeImg() {
+            document.getElementById(`${name.name}-btn`).classList.toggle("red");
+            document.getElementById(`${name.name}-btn`).classList.toggle("black");
+        }
+        document.getElementById(`${name.name}-btn`).addEventListener("click", changeLikeImg)
+
+}
+
+createCard(bichonFrise);
+createCard(miniPoodle);
---
+++ HTML, CSS and JS/FurEver Friends/info.js
@@ -0,0 +1,25 @@
+const bichonFrise = {
+    name: "Snowball",
+    breed: "Bichon Frise",
+    age: "3 yr",
+    height: "25 cm",
+    weight: "5.4 kg",
+    distance: "1.5 km away",
+    description: "Snowball is a sweet and affectionate Bichon Frise with a soft, white curly coat that gives her a fluffy, cloud-like appearance.",
+    src: "img/pic/frise-bichon.jpg",
+    gender: "female"
+};
+
+const miniPoodle = {
+    name: "Ginger",
+    breed: "Miniature Poodle",
+    age: "4 yr",
+    height: "30 cm",
+    weight: "6.3 kg",
+    distance: "2 km away",
+    description: "Ginger is a charming and elegant miniature poodle with a soft, curly coat that's a beautiful mix of creamy white with hints of apricot on her ears.",
+    src: "img/pic/poodle-miniature.jpg",
+    gender: "male"
+};
+
+export {bichonFrise, miniPoodle}
