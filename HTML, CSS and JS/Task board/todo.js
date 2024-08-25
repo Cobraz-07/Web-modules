a9f091cf9fb1dc7c6c4c161b0fce1e54edadbf79
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2024-08-25 03:09:10 +0200

    Job done, 4h+
    Finally got everything working, including localstorage

---
+++ HTML, CSS and JS/Task board/todo.js
@@ -0,0 +1,81 @@
+const form = document.getElementById("todo-form");
+const input1 = document.getElementById("todo-input-1");
+const input2 = document.getElementById("todo-input-2");
+const todolane = document.getElementById("todolane");
+const proglane = document.getElementById("proglane");
+const donelane = document.getElementById("donelane");
+const reviewlane = document.getElementById("reviewlane");
+const saveBtn = document.getElementById("save");
+const recoverBtn = document.getElementById("recover");
+const clearBtn = document.getElementById("clear");
+const clearDoneBtn = document.getElementById("clear-done");
+let task = [];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value1 = input1.value;
+  const value2 = input2.value;
+  if (!value1) return;
+  if (!value2) return;
+
+  const newTask = document.createElement("div");
+  newTask.classList.add("task");
+  newTask.setAttribute("draggable", "true");
+  newTask.innerHTML = `<h1 class="task-title">${value1}</h1>
+                       <p class="task-desc">${value2}</p>`;
+
+  newTask.addEventListener("dragstart", () => {
+    newTask.classList.add("is-dragging");
+  });
+  newTask.addEventListener("dragend", () => {
+    newTask.classList.remove("is-dragging");
+  });
+
+  todolane.appendChild(newTask);
+
+  input1.value = "";
+  input2.value = "";
+});
+
+function save() {
+  localStorage.clear();
+  localStorage.setItem("todo", todolane.innerHTML);
+  localStorage.setItem("prog", proglane.innerHTML);
+  localStorage.setItem("done", donelane.innerHTML);
+  localStorage.setItem("rev", reviewlane.innerHTML);
+};
+
+function recover() {
+  todolane.innerHTML = localStorage.getItem("todo");
+  proglane.innerHTML = localStorage.getItem("prog");
+  donelane.innerHTML = localStorage.getItem("done");
+  reviewlane.innerHTML = localStorage.getItem("rev");
+  task = document.querySelectorAll("div.task");
+  for (let i = 0; i < task.length; i++) {
+    task[i].addEventListener("dragstart", () => {
+      task[i].classList.add("is-dragging");
+    });
+    task[i].addEventListener("dragend", () => {
+      task[i].classList.remove("is-dragging");
+    });
+  };
+};
+
+function clear() {
+  localStorage.clear();
+  todolane.innerHTML = `<h3 class="heading">To do</h3>`;
+  proglane.innerHTML = `<h3 class="heading">In progress</h3>`;
+  donelane.innerHTML = `<h3 class="heading">Done</h3>`;
+  reviewlane.innerHTML = `<h3 class="heading">Under review</h3>`;
+};
+
+function clearDone() {
+  localStorage.setItem("done", `<h3 class="heading">Done</h3>`)
+  donelane.innerHTML = `<h3 class="heading">Done</h3>`;
+}
+
+saveBtn.addEventListener("click", save);
+recoverBtn.addEventListener("click", recover);
+clearBtn.addEventListener("click", clear);
+clearDoneBtn.addEventListener("click", clearDone);
+
