3b623a8b5bdd2cce4c78731c79580fd69afe6b64
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2024-08-26 02:28:52 +0200

    Update info.js
    Create all of the dogs info

--- HTML, CSS and JS/FurEver Friends/info.js
+++ HTML, CSS and JS/FurEver Friends/info.js
@@ -22,4 +22,64 @@ const miniPoodle = {
     gender: "male"
 };
 
-export {bichonFrise, miniPoodle}
+const rottweiler = {
+    name: "Max",
+    breed: "Rottweiler",
+    age: "1 yr",
+    height: "56 cm",
+    weight: "34 kg",
+    distance: "4 km away",
+    description: "Max is a playful and energetic Rottweiler puppy with a strong, stocky build that's already showing signs of the powerful adult dog he'll become.",
+    src: "img/pic/rottweiler.jpg",
+    gender: "male"
+};
+
+const sheepdogEnglish = {
+    name: "Oliver",
+    breed: "English Sheepdog",
+    age: "3 yr",
+    height: "61 cm",
+    weight: "36 kg",
+    distance: "1 km away",
+    description: "Oliver is a charming and hardworking English Sheepdog with a thick, shaggy coat that's a mix of white and gray.",
+    src: "img/pic/sheepdog-english.jpg",
+    gender: "male"
+};
+
+const spanielSussex = {
+    name: "Bella",
+    breed: "Sussex Spaniel",
+    age: "2 yr",
+    height: "38 cm",
+    weight: "20 kg",
+    distance: "5 km away",
+    description: "Bella is a sweet and devoted Sussex Spaniel with a long, low-slung body and a gorgeous, rich golden-liver coat that gleams in the sunlight.",
+    src: "img/pic/spaniel-sussex.jpg",
+    gender: "female"
+};
+
+const spanielWelsh = {
+    name: "Cooper",
+    breed: "Welsh Spaniel",
+    age: "1.5 yr",
+    height: "46 cm",
+    weight: "18 kg",
+    distance: "3.5 km away",
+    description: "Cooper is a spirited and friendly Welsh Springer Spaniel with a sleek, red-and-white coat that shines as he dashes through the grass.",
+    src: "img/pic/spaniel-welsh.jpg",
+    gender: "male"
+};
+
+const terrierAmerican = {
+    name: "Rocky",
+    breed: "American Terrier",
+    age: "2 yr",
+    height: "48 cm",
+    weight: "27 kg",
+    distance: "1 km away",
+    description: "Rocky is a strong and determined American Staffordshire Terrier with a muscular build and a short, glossy coat that’s a striking mix of brindle and white.",
+    src: "img/pic/terrier-american.jpg",
+    gender: "male"
+};
+
+export {bichonFrise, miniPoodle, rottweiler, sheepdogEnglish, spanielSussex, spanielWelsh, terrierAmerican};
