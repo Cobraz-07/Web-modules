56692894cff9142aeba176dd93de6caac4a541a6
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2024-08-28 00:27:46 +0200

    Update creator js
    Make names of functions, elements and parameters more descriptive

--- HTML, CSS and JS/FurEver Friends/CardCreator.js
+++ HTML, CSS and JS/FurEver Friends/CardCreator.js
@@ -3,35 +3,35 @@ const mainEl = document.getElementById("main");
 
 import {PetArray} from "./PetInfo.js";
 
-function createCard(name) {
-    const li = document.createElement("li");
-    li.classList.add("pet-card");
-    li.innerHTML = `<img src="${name.src}" class="pet-img" alt="${name.name} image" id="${name.name}-img">
-                <div class="text-div" id="${name.name}-text">
-                    <h1 class="name">${name.name}<img alt="" src="img/svg/${name.gender}_24dp_BLACK_FILL1_wght400_GRAD0_opsz24.svg" class="gender"></h1>
-                    <p class="breed">${name.breed}</p>
-                    <p class="distance"><img alt="" src="img/svg/location_on_24dp_D16D6A_FILL1_wght400_GRAD0_opsz24.svg" class="location">${name.distance}</p>
+function createCard(dog) {
+    const card = document.createElement("li");
+    card.classList.add("pet-card");
+    card.innerHTML = `<img src="${dog.src}" class="pet-img" alt="${dog.name} image" id="${dog.name}-img">
+                <div class="text-div" id="${dog.name}-text">
+                    <h1 class="name">${dog.name}<img alt="" src="img/svg/${dog.gender}_24dp_BLACK_FILL1_wght400_GRAD0_opsz24.svg" class="gender"></h1>
+                    <p class="breed">${dog.breed}</p>
+                    <p class="distance"><img alt="" src="img/svg/location_on_24dp_D16D6A_FILL1_wght400_GRAD0_opsz24.svg" class="location">${dog.distance}</p>
                 </div>
-                <button aria-label="Like button for ${name.name}" class="like-btn"><img alt="" id="${name.name}-like-btn" src="" class="black like"></button>`;
+                <button aria-label="Like button for ${dog.name}" class="like-btn"><img alt="" id="${dog.name}-like-btn" src="" class="black like"></button>`;
 
 
-    petList.appendChild(li);
+    petList.appendChild(card);
     
 function openPage() {
-    document.getElementById(`${name.name}-page`).classList.remove("hide");
+    document.getElementById(`${dog.name}-page`).classList.remove("hide");
     mainEl.classList.add("hide")
 };
 
-    document.getElementById(`${name.name}-img`).addEventListener("click", openPage);
-    document.getElementById(`${name.name}-text`).addEventListener("click", openPage);
+    document.getElementById(`${dog.name}-img`).addEventListener("click", openPage);
+    document.getElementById(`${dog.name}-text`).addEventListener("click", openPage);
 
         function changeLikeImg() {
-            document.getElementById(`${name.name}-like-btn`).classList.toggle("red");
-            document.getElementById(`${name.name}-like-btn`).classList.toggle("black");
-            document.getElementById(`${name.name}-big-like-btn`).classList.toggle("red");
-            document.getElementById(`${name.name}-big-like-btn`).classList.toggle("black");
+            document.getElementById(`${dog.name}-like-btn`).classList.toggle("red");
+            document.getElementById(`${dog.name}-like-btn`).classList.toggle("black");
+            document.getElementById(`${dog.name}-big-like-btn`).classList.toggle("red");
+            document.getElementById(`${dog.name}-big-like-btn`).classList.toggle("black");
         };
-        document.getElementById(`${name.name}-like-btn`).addEventListener("click", changeLikeImg);
+        document.getElementById(`${dog.name}-like-btn`).addEventListener("click", changeLikeImg);
 
 };
 
--- HTML, CSS and JS/FurEver Friends/PageCreator.js
+++ HTML, CSS and JS/FurEver Friends/PageCreator.js
@@ -3,62 +3,62 @@ const mainEl = document.getElementById("main");
 
 import {PetArray} from "./PetInfo.js";
 
-function createPage(name) {
-    const div = document.createElement("div");
-    div.classList.add("page");
-    div.classList.add("hide");
-    div.id = `${name.name}-page`;
-    div.innerHTML = `<button aria-label="Button for going back" class="back-btn"><img alt="" src="img/svg/arrow_back_24dp_000_FILL0_wght400_GRAD0_opsz24.svg" id="${name.name}-back" class="back-img"></button>
-        <button aria-label="Like button for ${name.name}" class="big-like-btn"><img alt="" src="" class="black" id="${name.name}-big-like-btn"></button>
-       <div class="big-img-div"><img alt="${name.name} image" src="${name.src}" class="big-img"></div> 
+function createPage(dog) {
+    const page = document.createElement("div");
+    page.classList.add("page");
+    page.classList.add("hide");
+    page.id = `${dog.name}-page`;
+    page.innerHTML = `<button aria-label="Button for going back" class="back-btn"><img alt="" src="img/svg/arrow_back_24dp_000_FILL0_wght400_GRAD0_opsz24.svg" id="${dog.name}-back" class="back-img"></button>
+        <button aria-label="Like button for ${dog.name}" class="big-like-btn"><img alt="" src="" class="black" id="${dog.name}-big-like-btn"></button>
+       <div class="big-img-div"><img alt="${dog.name} image" src="${dog.src}" class="big-img"></div> 
        <div class="bottom-section">
         <div class="info-div">
             <div class="dog-name-div">
-                <h1 class="big-name">${name.name}<img alt="" src="img/svg/male_24dp_BLACK_FILL1_wght400_GRAD0_opsz24.svg" class="big-gender"></h1>
-                <p class="breed">${name.breed}</p>
+                <h1 class="big-name">${dog.name}<img alt="" src="img/svg/male_24dp_BLACK_FILL1_wght400_GRAD0_opsz24.svg" class="big-gender"></h1>
+                <p class="breed">${dog.breed}</p>
             </div>
             <div class="location-div">
-                <p class="distance"><img alt="" src="img/svg/location_on_24dp_D16D6A_FILL1_wght400_GRAD0_opsz24.svg" class="location">${name.distance}</p>
+                <p class="distance"><img alt="" src="img/svg/location_on_24dp_D16D6A_FILL1_wght400_GRAD0_opsz24.svg" class="location">${dog.distance}</p>
             </div>
         </div>
         <div class="info-div between-spacing">
             <div class="pop-div">
                 <p class="pop-title">Age</p>
-                <p class="pop-text">${name.age}</p>
+                <p class="pop-text">${dog.age}</p>
             </div>
             <div class="pop-div">
                 <p class="pop-title">Height</p>
-                <p class="pop-text">${name.height}</p>
+                <p class="pop-text">${dog.height}</p>
             </div>
             <div class="pop-div">
                 <p class="pop-title">Weight</p>
-                <p class="pop-text">${name.weight}</p>
+                <p class="pop-text">${dog.weight}</p>
             </div>
         </div>
         <div class="desc-div">
-            <p class="desc">${name.description}</p>
+            <p class="desc">${dog.description}</p>
         </div>
        </div>
        <div class="btn-div">
-            <button aria-label="Donate button for ${name.name}" class="donate-btn">Donate<img alt="" src="img/svg/info_24dp_BLACK_FILL1_wght400_GRAD0_opsz24.svg" class="btn-icon"></button>
-            <button aria-label="Adopt button for ${name.name}" class="adopt-btn">Adopt<img alt="" src="img/svg/pets_24dp_FFF_FILL0_wght400_GRAD0_opsz24.svg" class="btn-icon"></button>
+            <button aria-label="Donate button for ${dog.name}" class="donate-btn">Donate<img alt="" src="img/svg/info_24dp_BLACK_FILL1_wght400_GRAD0_opsz24.svg" class="btn-icon"></button>
+            <button aria-label="Adopt button for ${dog.name}" class="adopt-btn">Adopt<img alt="" src="img/svg/pets_24dp_FFF_FILL0_wght400_GRAD0_opsz24.svg" class="btn-icon"></button>
         </div>`;
 
 
-       bodyEl.appendChild(div);
+       bodyEl.appendChild(page);
     
         function changeLikeImg() {
-            document.getElementById(`${name.name}-big-like-btn`).classList.toggle("red");
-            document.getElementById(`${name.name}-big-like-btn`).classList.toggle("black");
-            document.getElementById(`${name.name}-like-btn`).classList.toggle("red");
-            document.getElementById(`${name.name}-like-btn`).classList.toggle("black");
+            document.getElementById(`${dog.name}-big-like-btn`).classList.toggle("red");
+            document.getElementById(`${dog.name}-big-like-btn`).classList.toggle("black");
+            document.getElementById(`${dog.name}-like-btn`).classList.toggle("red");
+            document.getElementById(`${dog.name}-like-btn`).classList.toggle("black");
         };
-        document.getElementById(`${name.name}-big-like-btn`).addEventListener("click", changeLikeImg);
+        document.getElementById(`${dog.name}-big-like-btn`).addEventListener("click", changeLikeImg);
         function goBack() {
-            document.getElementById(`${name.name}-page`).classList.add("hide");
+            document.getElementById(`${dog.name}-page`).classList.add("hide");
             mainEl.classList.remove("hide")
         }
-        document.getElementById(`${name.name}-back`).addEventListener("click", goBack)
+        document.getElementById(`${dog.name}-back`).addEventListener("click", goBack)
 
 }
 
