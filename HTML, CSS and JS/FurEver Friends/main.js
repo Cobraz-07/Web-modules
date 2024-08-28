de8d2b6d4a30ca42ac8f7c462275814dad725e6b
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2024-08-29 01:35:50 +0200

    idek v3

--- HTML, CSS and JS/FurEver Friends/main.js
+++ HTML, CSS and JS/FurEver Friends/main.js
@@ -2,19 +2,6 @@ var isOnline = true;														// Can be used to check if device is in offlin
 var isWebAPK = window.matchMedia('(display-mode: standalone)').matches;		// Can be used to check if browser or webapk currently running
 console.log("[*] The app is running as a "+(isWebAPK?"WebAPK":"Browser-Page"));
 
-function checkOnlineStatus(){
-	isOnline = navigator.onLine;
-	console.log("[*] Connection status: "+(isOnline?"online":"offline"));
-	if (isOnline){
-		$("#connection abbr").html("&#128246;");
-		$("#connection abbr").attr("title","You are online!");
-	}
-	else {
-		$("#connection abbr").html("&#9888;");
-		$("#connection abbr").attr("title","You are offline!");
-	}
-}
-
 function init(){
 	// Register service worker
 	if ('serviceWorker' in navigator) {
@@ -29,21 +16,6 @@ function init(){
 	}
 	else console.log("[*] ServiceWorker not supported by your browser!");
 	
-	// Trigger install prompt for WebAPK
-	window.addEventListener("beforeinstallprompt",function(event){
-		console.log("[*] WebAPK install event fired!");
-		var btn = $("<button>install</button>");
-		$("body").append(btn);
-		btn.click(function(e){
-			event.prompt();
-			btn.remove();
-		});
-	});
-	
-	// Initialize online/offline detection
-	checkOnlineStatus();
-	window.addEventListener("online",checkOnlineStatus);
-	window.addEventListener("offline",checkOnlineStatus);
 }
 
 init();
